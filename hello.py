cb2aa48cd46c4d2f64d4a3a6bc4d81dd59d517b4
piotrmalyszka <piotrmalyszka1@wp.pl>
2020-03-28 19:42:33 +0100

    Koniec na dzis - gala

---
+++ hello.py
@@ -0,0 +1,45 @@
+##### PRZELICZNIK MIL NA KILOMETRY #####
+
+# kilometry = float(input("Podaj ilosc kilometrow: "))
+# mile = float(input("Podaj ilosc mil: "))
+
+
+# mile_na_kilometry = (mile * 1.61)
+# kilometry_na_mile = (kilometry * 0.61)
+
+# print("{mile} mil po konwersjii to: ".format(mile=mile), round(mile_na_kilometry , 2), "km")
+# print("{kilometry} km po konwersjii to: ".format(kilometry=kilometry) , round(kilometry_na_mile , 2) , "mil")
+
+
+
+####### PROSTE DZIALANIE ######## 
+
+# x = (input("Podaj x: "))
+# x = float(x)
+
+# y = (3 * (x ** 3) - 2 * (x ** 2) + 3 * x - 1)
+
+# print("Wynik tego działania wynosi: " , y)
+
+
+###### PROSTY KALKULATOR #######
+
+# a = float(input("Podaj wartość zmiennejprzecinkowej a: "))
+# b = float(input("Podaj wartość zmiennejprzecinkowej b: "))
+
+# suma = a + b
+# roznica = a - b
+# iloraz = a * b
+# iloczyn = a / b
+
+# print("Wynik dodawania wynosi: " , suma)
+# print("Wynik odejmowania wynosi: " , roznica)
+# print("Wynik mnozenia wynosi: " , iloraz)
+# print("Wynik dzielenia wynosi: " , round(iloczyn , 2))
+
+# print("\n KONIEC PROGRAMU") 
+
+
+
+
+
