164b687f4d04c9ffd28a673609b9844534e66292
piotrmalyszka <piotrmalyszka1@wp.pl>
2020-04-28 11:38:03 +0200

    Przerwa

---
+++ Zdalne/zdalne.py
@@ -0,0 +1,147 @@
+## PRZYKLAD INSTR WAR ##
+# x=1;
+# y=3;
+
+# if x>y:
+    # print(x)
+# elif x<y:
+    # print(y)
+
+## PETLA FOR ##
+
+# for x in range(1,6,1):
+    # print(str(x) + " ")
+
+## ZADANIE 1 ##
+
+# a = input("Podaj zdanie, w ktorym policze spacje: ")
+# x = a.count(' ')
+# print(x)
+
+## ZADANIE 2 ##
+
+# import sys
+
+# print("Podaj 1 liczbe: ")
+# a = sys.stdin.readline()
+# a=int(a)
+# print("Podaj 2 liczbe: ")
+# b = sys.stdin.readline()
+# b=int(b)
+# wynik=a*b
+# wynik=str(wynik)
+# print("Wynik mnożenia wynosi")
+# wynik = sys.stdout.write(wynik)
+
+## ZADANIE 4 ##
+
+# x = int(input("Podaj Liczbe: " ))
+# if x < 0:
+#     x *= -1
+# print(f"Wartosc bezwzgledna z podanej liczby wynosi {x}")
+
+## ZADANIE 5##
+# a = input("Podaj a: ")
+# b = input("Podaj b: ")
+# c = input("Podaj c: ")
+
+# a = int(a)
+# b = int(b)
+# c = int(c)
+
+# if 0 < a <=10 and a > b and  b > c:
+#     print("Warunek jest spełniony!")
+# else:
+#     print("Warunek nie jest spelniony!")   
+
+## ZADANIE 6 ##
+
+# for x in range(1,100):
+#     if x % 5 == 0:
+#         print(x)
+
+## ZADANIE 7 ##
+# a = input("Podaj 1 liczbe: ")
+# b = input("Podaj 2 liczbe: ")
+
+# a = int(a)
+# b = int(b)
+
+# print("Kwadrat a: " + str(a**2))
+# print("Kwadrat b: " + str(b**2))
+
+## ZADANIE 8 ##
+
+# lista = []
+
+# a = input("Podaj a: ")
+# b = input("Podaj b: ")
+# a = int(a)
+# b = int(b)
+
+# lista.append(a)
+# lista.append(b)
+
+# print(lista)
+
+## ZADANIE 9 ##
+
+# a = input("Podaj liczbe wielocyfrowa: ")
+# a = int(a)
+
+## ZADANIE 10 ##
+# import sys
+
+# for i in [1, 2, 3, 4, 5]:
+#     for j in[1, 2, 3, 4, 5]:
+#         if i == 3:
+#             sys.stdout.write('H')
+#         else:
+#             if j in[1, 5]:
+#                 sys.stdout.write('H')
+#             else:
+#                 sys.stdout.write(' ')
+#     sys.stdout.write('\n')
+
+## ZADANIE 12 ##
+
+# for y in range(1,11):
+#     for x in range(1,11):
+#         print(x * y, end = "\t")
+#     print()
+    
+## ZADANIE 14 ##
+# import math
+
+# try:
+#     a = input("Podaj liczbe, z ktorej wylicze pierwiastek: ")
+#     a = int(a)
+#     wynik = math.sqrt(a)
+#     print("Pierwiastek wynosi: ",str(wynik))
+# except ValueError:
+#     print("Blad! Podales liczbe ujemna!")
+
+## ZADANIE 15 ##
+
+# try:
+#     a = input("Podaj liczbe calkowita: ")
+#     a = int(a)
+#     print("Twoja liczba to: ", a)
+# except ValueError:
+#     print("Blad! Podales znak zamiast liczby!")
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
