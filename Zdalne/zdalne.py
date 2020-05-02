b5230a004235f3a6a753d1c76f1ad9816c0ae0ff
piotrmalyszka <piotrmalyszka1@wp.pl>
2020-05-02 19:01:24 +0200

    teams

--- Zdalne/zdalne.py
+++ Zdalne/zdalne.py
@@ -1,118 +1,107 @@
-## PRZYKLAD INSTR WAR ##
-# x=1;
-# y=3;
-
-# if x>y:
-    # print(x)
-# elif x<y:
-    # print(y)
-
-## PETLA FOR ##
-
-# for x in range(1,6,1):
-    # print(str(x) + " ")
 
 ## ZADANIE 1 ##
 
-# a = input("Podaj zdanie, w ktorym policze spacje: ")
-# x = a.count(' ')
-# print(x)
+a = input("Podaj zdanie, w ktorym policze spacje: ")
+x = a.count(' ')
+print(x)
 
 ## ZADANIE 2 ##
 
-# import sys
+import sys
 
-# print("Podaj 1 liczbe: ")
-# a = sys.stdin.readline()
-# a=int(a)
-# print("Podaj 2 liczbe: ")
-# b = sys.stdin.readline()
-# b=int(b)
-# wynik=a*b
-# wynik=str(wynik)
-# print("Wynik mnożenia wynosi")
-# wynik = sys.stdout.write(wynik)
+print("Podaj 1 liczbe: ")
+a = sys.stdin.readline()
+a=int(a)
+print("Podaj 2 liczbe: ")
+b = sys.stdin.readline()
+b=int(b)
+wynik=a*b
+wynik=str(wynik)
+print("Wynik mnożenia wynosi")
+wynik = sys.stdout.write(wynik)
 
 ## ZADANIE 4 ##
 
-# x = int(input("Podaj Liczbe: " ))
-# if x < 0:
-#     x *= -1
-# print(f"Wartosc bezwzgledna z podanej liczby wynosi {x}")
+x = int(input("Podaj Liczbe: " ))
+if x < 0:
+    x *= -1
+print(f"Wartosc bezwzgledna z podanej liczby wynosi {x}")
 
 ## ZADANIE 5##
-# a = input("Podaj a: ")
-# b = input("Podaj b: ")
-# c = input("Podaj c: ")
+a = input("Podaj a: ")
+b = input("Podaj b: ")
+c = input("Podaj c: ")
 
-# a = int(a)
-# b = int(b)
-# c = int(c)
+a = int(a)
+b = int(b)
+c = int(c)
 
-# if 0 < a <=10 and a > b and  b > c:
-#     print("Warunek jest spełniony!")
-# else:
-#     print("Warunek nie jest spelniony!")   
+if 0 < a <=10 and a > b and  b > c:
+    print("Warunek jest spełniony!")
+else:
+    print("Warunek nie jest spelniony!")   
 
 ## ZADANIE 6 ##
 
-# for x in range(1,100):
-#     if x % 5 == 0:
-#         print(x)
+for x in range(1,100):
+    if x % 5 == 0:
+        print(x)
 
 ## ZADANIE 7 ##
-# a = input("Podaj 1 liczbe: ")
-# b = input("Podaj 2 liczbe: ")
+a = input("Podaj 1 liczbe: ")
+b = input("Podaj 2 liczbe: ")
 
-# a = int(a)
-# b = int(b)
+a = int(a)
+b = int(b)
 
-# print("Kwadrat a: " + str(a**2))
-# print("Kwadrat b: " + str(b**2))
+print("Kwadrat a: " + str(a**2))
+print("Kwadrat b: " + str(b**2))
 
 ## ZADANIE 8 ##
 
-# lista = ['j' , "Asia"]
+lista = ['j' , "Asia"]
 
-# a = input("Podaj a: ")
-# b = input("Podaj b: ")
-# a = int(a)
-# b = int(b)
+a = input("Podaj a: ")
+b = input("Podaj b: ")
+a = int(a)
+b = int(b)
 
-# lista.append(a)
-# lista.append(b)
-# print(lista)
+while (a not in lista and b not in lista):
+    lista.append(a)
+    lista.append(b)
+print(lista)
 
-
+## ZADANIE 9 ##
 ## ZADANIE 10 ##
 ## ZADANIE 11 ##
 
 ## ZADANIE 12 ##
 
-# for y in range(1,11):
-#     for x in range(1,11):
-#         print(x * y, end = "\t")
-#     print()
+for y in range(1,11):
+    for x in range(1,11):
+        print(x * y, end = "\t")
+    print()
     
 ## ZADANIE 14 ##
-# import math
+import math
 
-# try:
-#     a = input("Podaj liczbe, z ktorej wylicze pierwiastek: ")
-#     a = int(a)
-#     wynik = math.sqrt(a)
-#     print("Pierwiastek wynosi: ",str(wynik))
-# except ValueError:
-#     print("Blad! Podales liczbe ujemna!")
+try:
+    a = input("Podaj liczbe, z ktorej wylicze pierwiastek: ")
+    a = int(a)
+    wynik = math.sqrt(a)
+    wynik = round(wynik,2)
+    print("Pierwiastek wynosi: ",str(wynik))
+except ValueError:
+    print("Blad! Podales liczbe ujemna!")
 
 ## ZADANIE 15 ##
 
-# try:
-#     a = input("Podaj liczbe calkowita: ")
-#     a = int(a)
-#     print("Twoja liczba to: ", a)
-# except ValueError:
-#     print("Blad! Podales znak zamiast liczby!")
+try:
+    a = input("Podaj liczbe calkowita: ")
+    a = int(a)
+    print("Twoja liczba to: ", a)
+except ValueError:
+    print("Blad! Podales znak zamiast liczby!")
 
 
            
